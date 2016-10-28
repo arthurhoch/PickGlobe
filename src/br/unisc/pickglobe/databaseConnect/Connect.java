dc732831640a7a142f8809d2dabe596d03ac5368
will <will@10.161.14.46>
2016-10-28 20:59:07 -0200

    Esqueleto do projeto

---
+++ src/br/unisc/pickglobe/databaseConnect/Connect.java
@@ -0,0 +1,26 @@
+package br.unisc.pickglobe.databaseConnect;
+
+/**
+ *
+ * @author will
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    
+    public static Connection Connect(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/nomebase?"
+                            + "user=root&password=");
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Seguinte erro ao tentar conectar: "+ex.getMessage());
+            return null;
+        }
+    }
+}
